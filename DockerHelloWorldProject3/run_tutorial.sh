e46238bff185eedd7402e5b836121a1a6cd25cde
Steven Carpenter <steven.carpenter@orbitfab.com>
2024-05-14 14:23:45 -0600

    demo #3 is working now!!

--- DockerHelloWorldProject3/run_tutorial.sh
+++ DockerHelloWorldProject3/run_tutorial.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-
+set -x
 #Sanity check
 echo $DO_DEMO
+
+printf "maybe copying to write directoy now!?"
 
 if [ "$DO_DEMO" = "1" ] 
 then
 
 # need to copy to $VAR_DIR		
 	cp  /docker-tutorial-hello/demo_files/read.txt /write_directory/read.txt
+	printf "copying to write directoy now!"
 	
 fi
 
-./docker-tutorial-hello/hello3 $VAR_DIR $VAR1 $VAR2 $VAR3
+./hello3 $VAR_DIR $VAR1 $VAR2 $VAR3
